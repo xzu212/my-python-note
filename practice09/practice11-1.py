95a164901501166b33b016acc5bab27cc6f641b1
xzu <15076012537@163.com>
2022-11-03 10:39:56 +0800

    Add files via upload

---
+++ practice09/practice11-1.py
@@ -0,0 +1,7 @@
+import json
+
+number = input("What is your favorite number? ")
+
+with open('favorite_number.json', 'w') as f:
+    json.dump(number, f)
+    print("Thanks! I'll remember that.")
---
+++ practice09/practice13.py
@@ -0,0 +1,40 @@
+import json
+
+
+def get_stored_username():
+    """获取存储的用户名——如果存储了。"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """提示用户输入用户名。"""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+
+def greet_user():
+    """提示用户输入用户名。"""
+    username = get_stored_username()
+    if username:
+        correct = input(f"Are you {username}? (y/n) ")
+        if correct == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+
+greet_user()
